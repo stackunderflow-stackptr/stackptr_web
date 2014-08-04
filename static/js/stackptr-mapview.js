b6e71b0224bb1f50ab24180d378d5079ab3303b1
Geordie Millar <gm@stackunderflow.com>
2014-08-04 15:58:50 +0930

    change tile provider to support retina tiles

--- static/js/stackptr-mapview.js
+++ static/js/stackptr-mapview.js
@@ -15,13 +15,22 @@ $(document).ready(function() {
 	
 	map = L.map('map-canvas').setView([-34.929, 138.601], 13);
 	
-	L.tileLayer('https://otile{s}-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19,
-    subdomains: '1234',
-	detectRetina: true,
-	}).addTo(map);
+	//L.tileLayer('https://otile{s}-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+	//    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+	//    maxZoom: 19,
+	//    subdomains: '1234',
+	//detectRetina: true,
+	//}).addTo(map);
 	
+	var tp = "ls";
+	if(L.Browser.retina) {
+		 tp = "lr";
+	};
+    L.tileLayer('https://tiles.lyrk.org/'+tp+'/{z}/{x}/{y}?apikey=049cf4528ba7472b8f5189860293b96c', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://geodienste.lyrk.de/copyright">Lizenzinformationen</a>, Tiles by <a href="http://geodienste.lyrk.de/">Lyrk</a>',
+        maxZoom: 18
+    }).addTo(map);
+
 	setupDraw();
 
 
