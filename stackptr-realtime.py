8cef7235736d2e19493580d7259243ce26ff3a75
Geordie Millar <gm@stackunderflow.com>
2014-12-18 19:07:48 +1030

    stackptr-realtime now using the ORM

--- stackptr-realtime.py
+++ stackptr-realtime.py
@@ -3,34 +3,38 @@ from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession
 from autobahn.wamp.exception import ApplicationError
 
-#from models import *
+from models import *
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 
 import ConfigParser
 config = ConfigParser.ConfigParser()
-config.read("/stackptr/stackptr.conf") # FIXME
+config.read("../stackptr.conf")
+
 
 db = create_engine(config.get("database", "uri"))
-#db.echo = True
-md = MetaData(db)
-AuthTicket = Table('auth_ticket', md, autoload=True)
+db.session = sessionmaker(bind=db)()
+
+
+#md = MetaData(db)
+#AuthTicket = Table('auth_ticket', md, autoload=True)
 
 class StackPtrAuthenticator(ApplicationSession):
 	@inlineCallbacks
 	def onJoin(self, details):
 		def authenticate(realm, authid, ticket):
 			try:
-				conn = db.connect()
-				res = conn.execute(text("select * from auth_ticket where key = :key and userid = :authid"), key=ticket, authid=authid).fetchone()
+				res = db.session.query(AuthTicket).filter_by(key=ticket, userid=authid).first()
 				if res:
-					conn.execute(text("delete from auth_ticket where key = :key"), key=ticket)
+					db.session.delete(res)
+					db.session.commit()
 					#fixme: check date
 					return 'user'
 				else:
 					raise ApplicationError("invalid-ticket", "invalid ticket %s" % ticket)
 			except Exception as e:
 				print e
+				raise e
 		
 		try:
 			yield self.register(authenticate, 'com.stackptr.authenticate')
@@ -53,8 +57,7 @@ class StackPtrAuthorizer(ApplicationSession):
 		requser = uri.split(".")[3]
 		
 		if (action == 'subscribe' and reqpath == 'com.stackptr.user'):		
-			conn = db.connect()
-			res = conn.execute(text("select * from follower where follower = :user and following = :requser and confirmed = 1"), user=user, requser=requser).fetchone()
+			res = db.session.query(Follower).filter_by(follower=user,following=requser,confirmed=1).first()
 			if res:
 				#print "accepted sub request: %s %s %s" % (session, uri, action)
 				return True
