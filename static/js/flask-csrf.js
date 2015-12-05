5d10ae38443cb2977650fa70108969e090491683
Geordie Millar <gm@stackunderflow.com>
2015-12-05 13:43:49 +1100

    Remove flask-csrf.js

--- static/js/flask-csrf.js
+++
@@ -1,9 +0,0 @@
-var csrftoken = $('meta[name=csrf-token]').attr('content')
-
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken)
-        }
-    }
-})
