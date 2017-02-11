3cdfe2557e2154d8997004712ae8ac02be423575
Geordie Millar <gm@stackunderflow.com>
2017-02-11 12:18:03 +1100

    Select from a set of colours, and select darker ones

--- static/js/stackptr-mapview.js
+++ static/js/stackptr-mapview.js
@@ -323,7 +323,11 @@ app.controller("StackPtrMap", ['$scope', '$cookies', '$http', '$interval', 'leaf
 			var img = new Image();
 			img.onload = function() {
 				var colourThief = new ColorThief();
-				var colour = colourThief.getColor(img);
+				var colourPalette = colourThief.getPalette(img,8).filter(function(e) {
+					var lum = e[0] + e[1] + e[2];
+					return lum < (127*3);
+				});
+				var colour = colourPalette.length > 0 ? colourPalette[0] : colourThief.getColor(img);
 				$scope.paths[markerId].color = rgb2hash(colour[0], colour[1], colour[2]);
 			};
 			img.crossOrigin = 'Anonymous';
